6472eb32780810fc20afa204171fa50a465542aa
conniedaisy <conniedcheung@gmail.com>
2016-03-18 14:59:17 -0700

    Create and render nested React components

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // TODO
+var Cucumbers = () => (
+  <li>Cucumbers</li>
+);
+
+var Kale = () => (
+  <li>Kale</li>
+);
+
+var GroceryList = () => (
+  <div>
+  <h2>My Grocery List</h2>
+    <ul>
+      <Cucumbers />
+      <Kale />
+    </ul>
+  </div>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById('app'));
