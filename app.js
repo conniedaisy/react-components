5944a6de476855502a167356d54d46a42f26b329
conniedaisy <conniedcheung@gmail.com>
2016-03-18 15:04:25 -0700

    Implement Component Properties aka props

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 // TODO
+var groceryItems = ['Milk', 'Chia', 'Tofu'];
+
 var Cucumbers = () => (
   <li>Cucumbers</li>
 );
@@ -7,14 +9,15 @@ var Kale = () => (
   <li>Kale</li>
 );
 
-var GroceryList = () => (
+var GroceryList = (props) => (
   <div>
   <h2>My Grocery List</h2>
     <ul>
-      <Cucumbers />
-      <Kale />
+      <li>{props.items[0]}</li>
+      <li>{props.items[1]}</li>
+      <li>{props.items[2]}</li>
     </ul>
   </div>
 );
 
-ReactDOM.render(<GroceryList />, document.getElementById('app'));
+ReactDOM.render(<GroceryList items={groceryItems}/>, document.getElementById('app'));
