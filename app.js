bbe7f19d2e3298220377780f27cf06939ec10f00
conniedaisy <conniedcheung@gmail.com>
2016-03-18 15:09:05 -0700

    Implement click event handler

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 // TODO
 var groceryItems = ['Milk', 'Chia', 'Tofu'];
 
-var Cucumbers = () => (
-  <li>Cucumbers</li>
-);
+var GroceryList = (props) => {
 
-var Kale = () => (
-  <li>Kale</li>
-);
+  var onListItemClick = (event) => {
+    console.log('Buy this!');
+  };
 
-var GroceryList = (props) => (
-  <div>
-  <h2>My Grocery List</h2>
-    <ul>
-      <li>{props.items[0]}</li>
-      <li>{props.items[1]}</li>
-      <li>{props.items[2]}</li>
-    </ul>
-  </div>
-);
+  return (
+    <div>
+    <h2>My Grocery List</h2>
+      <ul>
+        <li onClick={onListItemClick}>{props.items[0]}</li>
+        <li>{props.items[1]}</li>
+        <li>{props.items[2]}</li>
+      </ul>
+    </div>
+  );  
+};
 
 ReactDOM.render(<GroceryList items={groceryItems}/>, document.getElementById('app'));
